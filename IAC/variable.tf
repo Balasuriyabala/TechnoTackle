c006944c6f35f773b848e73b251cdd46f36319c5
Balasuriya <111849019+Balasuriyabala@users.noreply.github.com>
2026-04-09 18:10:32 +0530

    Add files via upload

---
+++ IAC/variable.tf
@@ -0,0 +1,40 @@
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_a_cidr" {
+  description = "CIDR block for public subnet A"
+  type        = string
+  default     = "10.0.1.0/24"
+} 
+
+variable "private_subnet_a_cidr" {
+  description = "CIDR block for private subnet A"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_b_cidr" {
+  description = "CIDR block for private subnet B"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
+variable "instance_class" {
+  default = "db.t3.micro"
+}
+
+variable "engine" {
+  default = "postgres"
+}
+
+variable "username" {
+    default = "postgres"
+}
+
+variable "password" {
+    default = "postgres@123!" 
+}
+  
